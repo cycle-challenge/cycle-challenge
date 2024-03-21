d679c480eac6c7b19343d1e264562a65f2deed62
takeny1998 <89298198+takeny1998@users.noreply.github.com>
2024-03-21 23:21:20 +0900

    feat(place): 관광지 세부정보 표시 기능 구현
    - ViewModel에 세부정보 얻기 Singleton으로 연결
    - 관련 UI 작성 (lib/ui/place/place_sheet_widget.dart)
    - 복사, 링크 이동 라이브러리 설치

--- lib/ui/place/place_sheet_widget.dart
+++ lib/ui/place/place_sheet_widget.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_linkify/flutter_linkify.dart';
+import 'package:ntp/ntp.dart';
 import 'package:provider/provider.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:yeohaeng_ttukttak/data/models/place/place_detail.dart';
 import 'package:yeohaeng_ttukttak/data/models/place_type.dart';
 import 'package:yeohaeng_ttukttak/states/bottom_sheet_state.dart';
 import 'package:yeohaeng_ttukttak/states/place_view_model.dart';
@@ -48,19 +53,164 @@ class PlaceViewSheet extends StatelessWidget {
                   ? null
                   : const BorderRadius.vertical(top: Radius.circular(20))),
           duration: const Duration(milliseconds: 400),
-          child: isExpanded ? PlaceDetailView() : PlaceSImpleView(),
+          child: isExpanded ? PlaceDetailView() : PlaceSimpleView(),
         ));
   }
 }
 
 class PlaceDetailView extends StatelessWidget {
+  Future<DateTime> _getPlaceDetail(BuildContext context) async {
+    await context.read<PlaceViewModel>().getDetail();
+    return await NTP.now();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return SizedBox();
+    return FutureBuilder(
+      future: _getPlaceDetail(context),
+      builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+        if (!snapshot.hasData)
+          return const Center(child: CircularProgressIndicator());
+
+        DateTime now = snapshot.data;
+
+        PlaceDetail? detail =
+            context.watch<PlaceViewModel>().selectedPlace?.detail;
+
+        if (detail == null) return const SizedBox();
+
+        TextStyle? bodyLarge = Theme.of(context).textTheme.bodyLarge;
+
+        return Container(
+          width: MediaQuery.of(context).size.width * 1.0,
+          margin: const EdgeInsets.symmetric(vertical: 30.0),
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                if (detail.address != null)
+                  Container(
+                    margin: const EdgeInsets.only(bottom: 20.0),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Container(
+                            margin: const EdgeInsets.only(right: 10.0),
+                            child: const Icon(Icons.place_outlined)),
+                        Container(margin: EdgeInsets.only(right: 5.0),child: Text(detail.address!, style: bodyLarge)),
+                        Material(
+                          child: InkWell(
+                            borderRadius: BorderRadius.circular(5),
+                            onTap: () async {
+                              await Clipboard.setData(
+                                  ClipboardData(text: detail.address!));
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                  SnackBar(
+                                      backgroundColor: Theme.of(context).colorScheme.secondaryContainer,
+                                      duration: const Duration(milliseconds: 2000),
+                                      content: Text("성공적으로 복사되었습니다.",
+                                          style: bodyLarge)));
+                            },
+                            child: Container(
+                              padding:
+                              const EdgeInsets.symmetric(horizontal: 5.0),
+
+                              child: Text("복사",
+                                  style:
+                                  bodyLarge?.copyWith(color: Colors.blue)),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                if (detail.businessHours != null && detail.isOpeningNow != null)
+                  Container(
+                    margin: const EdgeInsets.only(bottom: 20),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Container(
+                            margin: const EdgeInsets.only(right: 10.0),
+                            child: const Icon(Icons.access_time_outlined)),
+                        Text(detail.isOpeningNow! ? "영업 중 · " : "영업 종료 · ",
+                            style: bodyLarge?.copyWith(
+                                fontWeight: FontWeight.w600)),
+                        Text(detail.businessHours![now.weekday - 1],
+                            style: bodyLarge)
+                      ],
+                    ),
+                  ),
+                if (detail.siteURL != null)
+                  Container(
+                    margin: const EdgeInsets.only(bottom: 20),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Container(
+                            margin: const EdgeInsets.only(right: 10.0),
+                            child: const Icon(Icons.language_outlined)),
+                        Linkify(
+                            onOpen: (link) async {
+                              if (!await launchUrl(Uri.parse(link.url))) {
+                                throw Exception('Could not launch ${link.url}');
+                              }
+                            },
+                            text: detail.siteURL!,
+                            style: bodyLarge?.copyWith(
+                                color: Colors.blue,
+                                decorationColor: Colors.blue)),
+                      ],
+                    ),
+                  ),
+                if (detail.phoneNumber != null)
+                  Container(
+                    margin: const EdgeInsets.only(bottom: 20),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Container(
+                            margin: const EdgeInsets.only(right: 10.0),
+                            child: const Icon(Icons.phone_outlined)),
+                        Container(
+                            margin: const EdgeInsets.only(right: 10.0),
+                            child: Text(detail.phoneNumber!, style: bodyLarge)),
+                        Material(
+                          child: InkWell(
+                            borderRadius: BorderRadius.circular(5),
+                            onTap: () async {
+                              await Clipboard.setData(
+                                  ClipboardData(text: detail.phoneNumber!));
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                  SnackBar(
+                                    backgroundColor: Theme.of(context).colorScheme.secondaryContainer,
+                                      duration: const Duration(milliseconds: 2000),
+                                      content: Text("성공적으로 복사되었습니다.",
+                                          style: bodyLarge)));
+                            },
+                            child: Container(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 5.0),
+                              child: Text("복사",
+                                  style:
+                                      bodyLarge?.copyWith(color: Colors.blue)),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  )
+              ],
+            ),
+          ),
+        );
+      },
+    );
   }
 }
 
-class PlaceSImpleView extends StatelessWidget {
+class PlaceSimpleView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     PlaceModel? place = context.read<PlaceViewModel>().selectedPlace;
