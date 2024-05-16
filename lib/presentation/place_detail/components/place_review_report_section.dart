255b9a45b98fe4aa4db6f8c6522a99b771e22780
takeny1998 <89298198+takeny1998@users.noreply.github.com>
2024-05-16 14:52:44 +0900

    refactor(place_detail): 리뷰 그래프 직접 구현 & 기존 종속성 제거
    - 복잡한 fl_chart 라이브러리를 제거하고, 직접 리뷰 그래프를 구현

--- lib/presentation/place_detail/components/place_review_report_section.dart
+++ lib/presentation/place_detail/components/place_review_report_section.dart
@@ -1,7 +1,5 @@
-import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
-import 'package:yeohaeng_ttukttak/utils/function.dart';
 
 class PlaceReviewReportSection extends StatefulWidget {
   final List<double> ratings;
@@ -14,71 +12,12 @@ class PlaceReviewReportSection extends StatefulWidget {
 }
 
 class _PlaceReviewReportSectionState extends State<PlaceReviewReportSection> {
-  Widget getTitles(double value, TitleMeta meta) {
-    final textTheme = Theme.of(context).textTheme;
-
-    return SideTitleWidget(
-        angle: 4.7,
-        axisSide: meta.axisSide,
-        space: 4,
-        child: Center(
-            child: Text('${value.toInt().toString()}점',
-                style: textTheme.bodyMedium
-                    ?.copyWith(fontWeight: FontWeight.w600))));
-  }
-
-  Widget getTopTitles(double value, TitleMeta meta) {
-    final textTheme = Theme.of(context).textTheme;
-    final colorScheme = Theme.of(context).colorScheme;
-
-    return SideTitleWidget(
-        angle: 4.7,
-        axisSide: meta.axisSide,
-        space: 4,
-        child: Center(
-            child: Text(widget.ratings[(5 - value).toInt()].toInt().toString(),
-                style: textTheme.bodyMedium
-                    ?.copyWith(color: colorScheme.outline))));
-  }
-
-  FlTitlesData get titlesData => FlTitlesData(
-        show: true,
-        bottomTitles: AxisTitles(
-          sideTitles: SideTitles(
-            showTitles: true,
-            reservedSize: 40,
-            getTitlesWidget: getTitles,
-          ),
-        ),
-        leftTitles: const AxisTitles(
-          sideTitles: SideTitles(showTitles: false),
-        ),
-        topTitles: AxisTitles(
-          sideTitles: SideTitles(
-              showTitles: true,
-              reservedSize: 30,
-              getTitlesWidget: getTopTitles),
-        ),
-        rightTitles: const AxisTitles(
-          sideTitles: SideTitles(showTitles: false),
-        ),
-      );
-
-  List<BarChartGroupData> createBarChartGroupData(List<double> ratings) {
-    return ratings
-        .mapWithIndex((e, index) => BarChartGroupData(x: 5 - index, barRods: [
-              BarChartRodData(
-                  toY: e,
-                  color: Colors.amber,
-                  backDrawRodData: BackgroundBarChartRodData(
-                      show: true, toY: 10, color: Colors.grey.shade800))
-            ]));
-  }
-
   @override
   Widget build(BuildContext context) {
     final textTheme = Theme.of(context).textTheme;
     final colorScheme = Theme.of(context).colorScheme;
+
+    final int reviewCounts = widget.ratings.reduce((a, b) => a + b).toInt();
 
     return Container(
       height: 200,
@@ -103,16 +42,32 @@ class _PlaceReviewReportSectionState extends State<PlaceReviewReportSection> {
         const SizedBox(width: 10),
         const VerticalDivider(),
         Expanded(
-            child: RotatedBox(
-                quarterTurns: -3,
-                child: BarChart(BarChartData(
-                    titlesData: titlesData,
-                    groupsSpace: 20,
-                    alignment: BarChartAlignment.center,
-                    gridData: const FlGridData(show: false),
-                    borderData: FlBorderData(show: false),
-                    maxY: 10,
-                    barGroups: createBarChartGroupData(widget.ratings))))),
+            child: Column(children: [
+          for (int score = 5; score >= 1; score--) ...[
+            Builder(builder: (context) {
+              final double rating = widget.ratings[score - 1] / reviewCounts;
+
+              return Row(children: [
+                SizedBox(
+                    width: 36,
+                    child: Center(
+                        child: Text('$score점', style: textTheme.bodyMedium))),
+                Expanded(
+                    child: ClipRRect(
+                        borderRadius: BorderRadius.circular(8),
+                        child: LinearProgressIndicator(
+                            color: Colors.amber, minHeight: 8, value: rating))),
+                SizedBox(
+                    width: 36,
+                    child: Center(
+                        child: Text('${widget.ratings[score - 1].toInt()}개',
+                            style: textTheme.bodyMedium
+                                ?.copyWith(color: colorScheme.outline))))
+              ]);
+            }),
+            const SizedBox(height: 3)
+          ]
+        ]))
       ]),
     );
   }
